91efa87b6d0baa31bd8df6ae79ad961044a30d37
AlvaroSLuna <serranoalvaroluna@gmail.com>
2025-02-06 17:13:35 +0100

    Prueba en index y src

--- index.php
+++ index.php
@@ -1,11 +1,75 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ARJ Records</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
+
 <body>
-    
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-xs-12">
+                    <img src="" class="img-thumbnail" />
+                </div>
+                <div class="col-md-9 col-xs-12">
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+                        <button class="navbar-toggler" type="button" data-
+                            toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-
+                            expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav flex-column">
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Inicio</a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Novedades</a>
+                                </li>
+
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#"
+                                        id="menu1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-
+                                        expanded="false">Artistas</a>
+                                    <div class="dropdown-menu" aria-
+                                        labelledby="menu1">
+                                        <a class="dropdown-item" href="#">Manolo</a>
+                                        <a class="dropdown-item" href="#">El</a>
+                                        <a class="dropdown-item" href="#">Del</a>
+                                        <a class="dropdown-item" href="#">Bombo</a>
+                                    </div>
+                                </li>
+
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#"
+                                        id="menu1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-
+                                        expanded="false">Generos</a>
+                                    <div class="dropdown-menu" aria-
+                                        labelledby="menu2">
+                                        <a class="dropdown-item" href="#">Rock</a>
+                                        <a class="dropdown-item" href="#">Rap</a>
+                                        <a class="dropdown-item" href="#">Pop</a>
+                                        <a class="dropdown-item" href="#">Regaeton</a>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+    </header>
+
 </body>
+
 </html>
